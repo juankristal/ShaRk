9d8ab8973b534ad4f43bcbde89dd6bf6561771e0
Kominaru <j.ruza@udc.es>
2021-10-19 19:13:23 +0200

    rolling avg
    ye

--- sr.py
+++ sr.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import math
+from scipy.interpolate import make_interp_spline, BSpline
+import random
 
 class HitObject:
     def __init__(self,column,timestamp,lnend=0) -> None:
@@ -47,13 +49,53 @@ def obtainDensityCalculation(ho,bin_size):
     plt.show()
     return np.average(v)
 
+def obtainDensityCalculation2(ho,bin_size):
+    v=np.zeros(len(ho),dtype=int)
+    x=np.zeros(len(ho),dtype=int)
+    j=0
+    k=0
+    lastt=-1
+    for i in range(len(ho)):
+        # if ho[i].timestamp==lastt:
+        #     v[i]=-1
+        #     x[i]=-1
+        #     continue
+        # lastt=ho[i].timestamp
+        d=0
+        while ho[j].timestamp<(ho[i].timestamp-bin_size/2):
+            j+=1
 
-bin_size=500
+        while ho[k].timestamp<(ho[i].timestamp+bin_size/2) and k<len(ho)-1:
+            k+=1
 
+        v[i]=k-j
+        x[i]=ho[i].timestamp
+
+    return (x,v)
+
+
+bin_size=3000
 for m in os.listdir(maps_folder):
     with open(maps_folder+m,"r",encoding="utf8") as f:
         ho = obtainHitObjectArrayFromOsu(f)
-        print(obtainDensityCalculation(ho,bin_size))
-
-
+        x,y=obtainDensityCalculation2(ho,bin_size)
+        w=1000
+        y_roll=np.convolve(y, np.ones(w), 'same') / w
+        # xf=x[x!=-1]
+        # yf=y[y!=-1]
+        # xnew = np.linspace(xf.min(), xf.max(), num=xf.shape[0]*5) 
+        # print(xnew)
+        # spl = BSpline(xf, yf, k=311,extrapolate="periodic")
+        # smooth_y= spl(xnew)
+        # plt.plot(xnew[1:],smooth_y[1:],label=m)
+        r = random.random()
+        b = random.random()
+        g = random.random()
+  
+        color = (r, g, b)
+        plt.plot(x,y,c=color,alpha=0.3)
+        plt.plot(x,y_roll,label=m,c=color,linewidth=3)
+plt.legend()
+plt.show()
+    
 
