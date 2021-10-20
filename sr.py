56b80ccdc68e5b669df1c7ff4fd02c65bb2877f0
Kominaru <j.ruza@udc.es>
2021-10-20 21:09:11 +0200

    update draft manip + global model
    today i drank a liter of pilk

--- sr.py
+++ sr.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import math
+from numpy.lib.function_base import average
 from scipy.interpolate import make_interp_spline, BSpline
 import random
 
@@ -69,13 +70,12 @@ def obtainDensityCalculation2(ho,bin_size):
             k+=1
 
         v[i]=k-j
-        x[i]=ho[i].timestamp
 
-    return (x,v)
+    return v
 
 def obtainManipCalculation(ho,bin_size):
-    v=np.zeros(len(ho),dtype=int)       
-    x=np.zeros(len(ho),dtype=int)
+    v=np.zeros(len(ho))       
+    x=np.zeros(len(ho))
     j=0
     k=0
     for i in range(len(ho)):
@@ -87,62 +87,67 @@ def obtainManipCalculation(ho,bin_size):
 
         d=[1,1,1,1]
         for h in range(j,k):
-            if ho[h].column==0:
-                d[0]=d[0]+1
-            elif ho[h].column==1:
-                d[1]=d[1]+1
-            elif ho[h].column==2:
-                d[2]=d[2]+1
-            elif ho[h].column==3:
-                d[3]=d[3]+1
-            else: print(ho[h].column)
-        v[i]=np.var(d[:2])+np.var(d[2:])+np.var([sum(d[:2]),sum(d[2:])])
-        x[i]=ho[i].timestamp
+            d[ho[h].column]=d[ho[h].column]+1
+        
+        l_manip=min(d[:2])/max(d[:2])/(1+np.var(d[:2])) #[0,1]Closer to 1-> more masheable
+        r_manip=min(d[2:])/max(d[2:])/(1+np.var(d[2:]))
+        h_manip=min(sum(d[:2]),sum(d[2:]))/max(sum(d[:2]),sum(d[2:]))/(1+np.var([sum(d[:2]),sum(d[2:])]))
+        v[i]=np.average([l_manip,r_manip,h_manip]) #[0,1] Clpser to 1 -> easier to mash
+        # v[i]=(
+        #     np.var(d[:2]) * (min(d[:2])/max(d[:2]))
+        #    +np.var(d[2:]) * (min(d[2:])/max(d[2:]))
+        #    +np.var([sum(d[:2]),sum(d[2:])]) * (min(sum(d[:2]),sum(d[2:]))/max(sum(d[:2]),sum(d[2:])))
+        # )
+    return v
 
-    return (x,v)
 
+text=["suiren","bass drop","lolit","starfall","nhelv"]
+dns_bin_size=1000
+mnp_bin_size=1000
+w=100
+fig, (dens, manip, total)=plt.subplots(3,1,sharex=True)
 
-text=["azure", "lolit","bass drop"]
-bin_size=500
-
-fig, (dens, manip)=plt.subplots(2,1,sharex=True)
-
-i=3
+i=.9
 for m in os.listdir(maps_folder):
     if text!=[] and not any([t in m.lower() for t in text]): continue
     with open(maps_folder+m,"r",encoding="utf8") as f:
         ho = obtainHitObjectArrayFromOsu(f)
-        x,y=obtainDensityCalculation2(ho,bin_size)
-        w=200
-        y_roll=np.array([np.average(y[max(0,i-w//2):min(len(ho),i+w//2)]) for i in range(len(ho))])
+        x=np.array([h.timestamp for h in ho])
+
         r = random.random()
         b = random.random()
         g = random.random()
-
-
-
         color = (r, g, b)
-        dens.plot(x,y,c=color,alpha=0.3)
-        dens.plot(x,y_roll,label=m,c=color,linewidth=3)
 
-        x,mnp=obtainManipCalculation(ho,bin_size)
-        w=50
-        y_roll=np.array([np.average(mnp[max(0,i-w//2):min(len(ho),i+w//2)]) for i in range(len(ho))])
-        r = random.random()
-        b = random.random()
-        g = random.random()
+        dns=obtainDensityCalculation2(ho,dns_bin_size)
+        dns_roll=np.array([np.average(dns[max(0,i-w//2):min(len(ho),i+w//2)]) for i in range(len(ho))])
+       
+        dens.plot(x,dns,c=color,alpha=0.1)
+        dens.plot(x,dns_roll,label=m,c=color,linewidth=3)
+        dens.text(0,i,s=f"{m[:12]+'...'}Avg. diff= {np.average(dns_roll)}",horizontalalignment='left',
+     verticalalignment='center',
+     transform = dens.transAxes)
 
+        mnp=obtainManipCalculation(ho,mnp_bin_size)
+        mnp_roll=np.array([np.average(mnp[max(0,i-w//2):min(len(ho),i+w//2)]) for i in range(len(ho))])
+        manip.plot(x,mnp,c=color,alpha=0.1)
+        manip.text(0,i,s=f"{m[:12]+'...'}Avg. diff= {np.average(mnp_roll)}",horizontalalignment='left',
+     verticalalignment='center',
+     transform = manip.transAxes)
 
+        manip.plot(x,mnp_roll,label=m,c=color,linewidth=3)
+        ttl_raw=dns/mnp
+        ttl=dns_roll/mnp_roll
+        ttl_roll=np.array([np.average(ttl[max(0,i-w//2):min(len(ho),i+w//2)]) for i in range(len(ho))])
+        total.plot(x,ttl_raw,c=color,alpha=0.1)
+        total.text(0,i,s=f"{m[:12]+'...'}Avg. diff= {np.average(ttl)}",horizontalalignment='left',
+     verticalalignment='center',
+     transform = total.transAxes)
+        total.plot(x,ttl_roll,label=m,c=color,linewidth=3)
+        total.set_ylim(0,100)
 
-        color = (r, g, b)
-        manip.plot(x,mnp,c=color,alpha=0.3)
-        manip.text(2,i+1,s=f"{m[:25]+'...'}Total averaged difficulty= {np.average((0.5*mnp)+(y*0.5))}")
-        manip.plot(x,y_roll,label=m,c=color,linewidth=3)
-
-        i+=1
+        i-=0.1
 
 dens.legend()
-manip.legend()
 plt.show()
-    
 
