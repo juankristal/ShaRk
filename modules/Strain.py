c272376a237087490ca0d81c99c12e4903615ff5
Kominaru <j.ruza@udc.es>
2021-11-05 01:37:11 +0100

    add OH double jack supression

--- modules/Strain.py
+++ modules/Strain.py
@@ -44,7 +44,41 @@ def obtainStrainCalculation(ho):
 
                 # Jacks don't get supressed for graces obviously
                 if c == ho[i].column:
-                    w += jack_w*(100/distance)
+                    c1= (ho[i].column+1) % 2 if ho[i].column in [0,1] else (ho[i].column-1) % 2+2 #Different column in the same hand
+
+                    j=i #List index pointer for iteration
+                    n2=-1; n1=-1   #Notes that could make the jack [i n2] and [n n1] (n is the next note in c after i)
+                    
+                    while not j<0 and n1==-1:   #While we don't obtain both notes or run out of notes to check, go backward through the HitObjects list
+                        if ho[j].column==c1:    #If we find a note in the desired column, "roll" the elements of the "queue" adding the new found note
+                            n1=n2
+                            n2=j
+                        j-=1
+
+                    if j<0 and n1!=1: #If we ran out of notes before i and couldn't find two 
+                        j=i+1  #Start looking for the missing notes after i this time
+
+                        while j<len(ho) and (n1==-1 or n2==-1):     #While we don't obtain both this time or run out of notes to check, go forward through the HitObjects list
+                            if ho[j].column==c1:    #If we find a note in the desired column, update n2 and n1
+                                if n2==-1: n2=n1 #If we had already found one note in the previous while, lock n2
+                                n1=j
+                            j+=1
+
+                    if n2==-1 or n1==-1: supress=1 #If we didn't have enough notes, just don't supress for jacks
+                    else:
+                        j=n1+1
+                        while j<len(ho): #Else, start iterating forward (find next noteS after n1 in c1)
+                            if ho[j].column==c1:
+                                d1=abs(ho[i].timestamp-ho[n2].timestamp)+abs(ho[n].timestamp-ho[n1].timestamp)   #Old pair distance
+                                d2=abs(ho[i].timestamp-ho[n1].timestamp)+abs(ho[n].timestamp-ho[j].timestamp)   #New pair distance
+                                
+                                if d2>d1: break     #If it increased, curent pair is better
+                                else:       #Otherwise move the currrent pair to the new found one
+                                    n2=n1
+                                    n1=j
+                            j+=1
+                        supress=0.5**((1-s(abs(ho[i].timestamp-ho[n2].timestamp)))*(1-s(abs(ho[n].timestamp-ho[n1].timestamp))))
+                    w += jack_w*(100/distance)*supress
 
                 # Otherwise, supress based on how gracey it is
                 else:
